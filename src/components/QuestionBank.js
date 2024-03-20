33e93cd2da726b3e5ce720f39dfa06bafab192c4
Anish Joseph <anish@mobil80.com>
2024-03-20 17:11:56 +0530

    quiz-app commit

---
+++ src/components/QuestionBank.js
@@ -0,0 +1,64 @@
+const qBank = [
+  {
+    id: 1,
+    question: "What is the capital of Haryana?",
+    options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
+    answer: "Chandigarh",
+  },
+  {
+    id: 2,
+    question: "What is the capital of Punjab?",
+    options: ["Patiala", "Ludhiana", "Amritsar", "Chandigarh"],
+    answer: "Chandigarh",
+  },
+  {
+    id: 3,
+    question: "What is the capital of India?",
+    options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+    answer: "Delhi",
+  },
+  {
+    id: 4,
+    question: "What is the capital of Uttarakhand?",
+    options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
+    answer: "Dehradun",
+  },
+  {
+    id: 5,
+    question: "Which year did India win it's first World Cup?",
+    options: ["1998", "1983", "1975", "2000"],
+    answer: "1983",
+  },
+  {
+    id: 6,
+    question: "Mary Kom is specialized in which of the following sports?",
+    options: ["Cricket", "Hockey", "Boxing", "Chess"],
+    answer: "Boxing",
+  },
+  {
+    id: 7,
+    question: "What is capital of Uttar Pradesh?",
+    options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+    answer: "Lucknow",
+  },
+  {
+    id: 8,
+    question: "A group of crows is called:",
+    options: ["Parliament", "Murder", "Crowd", "Pack"],
+    answer: "Lucknow",
+  },
+  {
+    id: 9,
+    question: "What is capital of Uttar Pradesh?",
+    options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+    answer: "Lucknow",
+  },
+  {
+    id: 10,
+    question: "What is capital of Uttar Pradesh?",
+    options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+    answer: "Lucknow",
+  },
+];
+
+export default qBank;
